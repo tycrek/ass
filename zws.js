1e6148271967f92e8fa72db8dbe3a7d90a18ef98
tycrek <josh.moore@jmoore.dev>
2021-04-05 17:09:04 -0600

    make small

--- zws.js
+++ zws.js
@@ -1,11 +1,3 @@
 const { randomBytes } = require('crypto');
-const zeroWidthCap = '\u200B';
-const zeroWidthChars = [
-	zeroWidthCap,
-	'\u200C',
-	'\u200D',
-	'\u2060',
-	'\u180E'
-];
-
-module.exports = (size) => [...randomBytes(size)].map(byte => zeroWidthChars[+byte % zeroWidthChars.length]).join('').slice(1) + zeroWidthCap;
+const zeroWidthChars = ['\u200B', '\u200C', '\u200D', '\u2060', '\u180E'];
+module.exports = (size) => [...randomBytes(size)].map(byte => zeroWidthChars[+byte % zeroWidthChars.length]).join('').slice(1) + zeroWidthChars[0];
