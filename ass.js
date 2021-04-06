0effd769d2db83b9520356f2f3fd3810985e90c2
tycrek <josh.moore@jmoore.dev>
2021-04-05 20:01:10 -0600

    added domain stuff a.k.a. goddamit I'm dumb

--- ass.js
+++ ass.js
@@ -57,7 +57,7 @@ function startup() {
 		data[resourceId] = req.file;
 		saveData(data);
 
-		res.type('json').send({ resource: `http://lh:${process.env.PORT}/${resourceId}`, delete: `http://lh:${process.env.PORT}/delete/${req.file.filename}` });
+		res.type('json').send({ resource: `http://${process.env.DOMAIN}:${process.env.PORT}/${resourceId}`, delete: `http://${process.env.DOMAIN}:${process.env.PORT}/delete/${req.file.filename}` });
 	});
 
 	// View file
