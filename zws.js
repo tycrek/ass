1063cc6117e5cadb087ffebb9cbe469ad6d08288
tycrek <josh.moore@jmoore.dev>
2021-04-06 01:18:43 -0600

    APPLE FIX YOUR SHITTY DEVICES

--- zws.js
+++ zws.js
@@ -1,3 +1,3 @@
 const { randomBytes } = require('crypto');
-const zeroWidthChars = ['\u200B', '\u200C', '\u200D', '\u2060', '\u180E'];
+const zeroWidthChars = ['\u200B', '\u200C', '\u200D', '\u2060'];
 module.exports = (size) => [...randomBytes(size)].map(byte => zeroWidthChars[+byte % zeroWidthChars.length]).join('').slice(1) + zeroWidthChars[0];
