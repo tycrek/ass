4185af26decf5bece23f41f4a4a4b9c7053b0b44
tycrek <josh.moore@jmoore.dev>
2021-04-07 09:17:09 -0600

    test hardcoded shit

--- oEmbed.js
+++ oEmbed.js
@@ -17,6 +17,17 @@ module.exports = (url, file) =>
 
 function generateJson(url, width, height) {
 	return ({
+		"height": 718,
+		"html": "<iframe class=\"streamable-embed\" src=\"https://streamable.com/o/12txw7\" frameborder=\"0\" scrolling=\"no\" width=\"1080\" height=\"718\" allowfullscreen></iframe>",
+		"provider_name": "Streamable",
+		"provider_url": "https://streamable.com",
+		"thumbnail_url": "//cdn-cf-east.streamable.com/image/12txw7.jpg?Expires=1618007820&Signature=jIK07C3KE44XAjz5C-AVd4~RlXGrS5umUwpWnL-koDhDm4WlkqZw2YqA6n4PewLy0u2vem16gouBMzpU1aenr39zVqUp3tdz~1B1pdeQZWLQnQsXiXb3ljuFHZXF-CAloIXI-Lz-zBQeAQ-UJ2DvcU3g9LXtDVs-HtK3tN4PZBARnmNvnYhwPjxDkUHV295QGs8tPelxZ6k-ZTlAs8QvL6KG3eAHMVLmxzxX9LjpgoKCNfLmBB2~mua1nHR7IM2~C17i~L-le~VxoQYu3PrlLcMLcH7x968PgZXNUqWWcRBFO~0aMTO32iCxTunHxxnjg7SoqWsh2CKTt2M1z-Y7CQ__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ",
+		"title": "s",
+		"type": "video",
+		"version": "1.0",
+		"width": 1080
+	})
+	return ({
 		type: 'video',
 		version: '1.0',
 		width,
