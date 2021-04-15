db1b88fb5ab61c8b33e792586a0761bd5df326c6
tycrek <josh.moore@jmoore.dev>
2021-04-15 13:33:35 -0600

    minor cleanup

--- ogp.js
+++ ogp.js
@@ -53,7 +53,7 @@ class OpenGraph {
 	}
 
 	build() {
-		let view = {
+		return Mustache.render(html, {
 			github,
 
 			http: this.http,
@@ -63,15 +63,13 @@ class OpenGraph {
 			ogtype: this.type.includes('video') ? 'video.other' : 'image',
 			type: this.type.includes('video') ? 'video' : 'image',
 			ext: this.type.includes('video') ? '.mp4' : '',
-		};
 
-		view.title = (this.title.length != 0) ? `<meta property="og:title" content="${this.title}">` : '';
-		view.description = (this.description.length != 0) ? `<meta property="og:description" content="${this.description}">` : '';
-		view.site = (this.author.length != 0) ? `<meta property="og:site_name" content="${this.author}">` : '';
-		view.color = (this.color.length != 0) ? `<meta name="theme-color" content="${this.color}">` : '';
-		view.card = !this.type.includes('video') ? `<meta name="twitter:card" content="summary_large_image">` : '';
-
-		return Mustache.render(html, view);
+			title: (this.title.length != 0) ? `<meta property="og:title" content="${this.title}">` : '',
+			description: (this.description.length != 0) ? `<meta property="og:description" content="${this.description}">` : '',
+			site: (this.author.length != 0) ? `<meta property="og:site_name" content="${this.author}">` : '',
+			color: (this.color.length != 0) ? `<meta name="theme-color" content="${this.color}">` : '',
+			card: !this.type.includes('video') ? `<meta name="twitter:card" content="summary_large_image">` : '',
+		});
 	}
 }
 
