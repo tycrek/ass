c9e6d09bde7cfce8699b526df78385be5dc2bec4
tycrek <josh.moore@jmoore.dev>
2021-04-17 20:04:12 -0600

    stuff that was built at some point, code does not work atm

--- src/generators/zws.js
+++
@@ -1,3 +0,0 @@
-const { randomBytes } = require('crypto');
-const zeroWidthChars = ['\u200B', '\u200C', '\u200D', '\u2060'];
-module.exports = (size) => [...randomBytes(size)].map(byte => zeroWidthChars[+byte % zeroWidthChars.length]).join('').slice(1) + zeroWidthChars[0];
