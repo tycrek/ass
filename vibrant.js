c13b7896275ddd8d95fc2cff28c5e2a90b989757
tycrek <josh.moore@jmoore.dev>
2021-06-16 00:43:19 -0600

    Improved Vibrant colour options

--- vibrant.js
+++ vibrant.js
@@ -1,7 +1,11 @@
 const Vibrant = require('node-vibrant');
 const { path } = require('./utils');
+
+const COLOR_COUNT = 256;
+const QUALITY = 3;
+
 module.exports = (file) =>
 	new Promise((resolve, reject) =>
-		Vibrant.from(path(file.path)).getPalette()
-			.then((palette) => resolve(palette.Vibrant.hex))
+		Vibrant.from(path(file.path))
+			.maxColorCount(COLOR_COUNT).quality(QUALITY).getPalette()
 			.catch(reject));
