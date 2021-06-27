64428d97591853d44ca8fdeb8cf65e4800451c23
tycrek <josh.moore@jmoore.dev>
2021-06-27 10:51:13 -0600

    Suppress DeepSource rule

--- hash.js
+++ hash.js
@@ -8,6 +8,6 @@ module.exports = (file) =>
 	new Promise((resolve, reject) =>
 		toArray((fs.createReadStream(s3enabled ? path(diskFilePath, file.originalname) : path(file.path))))
 			.then((parts) => Buffer.concat(parts.map((part) => (Buffer.isBuffer(part) ? part : Buffer.from(part)))))
-			.then((buf) => crypto.createHash('sha1').update(buf).digest('hex'))
+			.then((buf) => crypto.createHash('sha1').update(buf).digest('hex')) // skipcq: JS-D003
 			.then(resolve)
 			.catch(reject));
