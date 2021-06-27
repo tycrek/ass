842a02fd0a336128b1111721d34e62c9aa911fa1
tycrek <josh.moore@jmoore.dev>
2021-06-27 10:51:35 -0600

    Double-quote Flameshot script for DeepSource

--- flameshot_example.sh
+++ flameshot_example.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-IMAGEPATH="$HOME/Pictures/" #where to hold the screenshots before they are deleted
-IMAGENAME="ass" #Not really important, tells flameshot what file to send and delete
-KEY="" #Enter auth key
-DOMAIN="" #your upload domain
+IMAGEPATH="$HOME/Pictures/" # Where to store screenshots before they're deleted
+IMAGENAME="ass" # Not really important, tells Flameshot what file to send and delete
+KEY="" # Your ass upload token
+DOMAIN="" # Your upload domain
 
-flameshot config -f $IMAGENAME #Make sure that flameshot names the file correctly
-flameshot gui -r -p $IMAGEPATH #Prompt the screenshot GUI
+flameshot config -f "$IMAGENAME" # Make sure that Flameshot names the file correctly
+flameshot gui -r -p "$IMAGEPATH" # Prompt the screenshot GUI
 
-#Upload the image and copy the response URL
+# Upload the image and copy the response URL
 URL=$(curl -X POST \
   -H "Content-Type: multipart/form-data" \
   -H "Accept: application/json" \
   -H "User-Agent: ShareX/13.4.0" \
   -H "Authorization: $KEY" \
-  -F "file=@$IMAGEPATH$IMAGENAME.png" https://$DOMAIN/ | grep -Po '(?<="resource":")[^"]+')
-echo $URL | xclip -sel clip
+  -F "file=@$IMAGEPATH$IMAGENAME.png" "https://$DOMAIN/" | grep -Po '(?<="resource":")[^"]+')
+echo "$URL" | xclip -sel clip
 
-rm $IMAGEPATH$IMAGENAME.png #Delete the image locally
-
-
-
+rm "$IMAGEPATH$IMAGENAME.png" # Delete the image locally
