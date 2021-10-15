bab0e42a0470b1f11d827227d366f6ee848d62eb
tycrek <josh.moore@jmoore.dev>
2021-10-15 13:49:35 -0600

    Converted view page to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,32 @@ module.exports = {
 	},
 	theme: {
 		extend: {
+			fontFamily: {
+				main: ['"Josefin Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif']
+			},
+			backgroundColor: {
+				'body': '#212121',
+			},
 			colors: {
-				'border': '#323232'
+				'content-bg': '#151515',
+				'primary': '#FD842D',
+				'primary-dim': '#B64D02',
+				'primary-dark': '#793301',
+				'link-hover': '#FD710D',
+				'link-active': '#DE5E02',
+				'text-primary': '#BDBDBD',
+			},
+			borderColor: {
+				'primary-dim': '#B64D02'
+			},
+			maxHeight: {
+				'half-port': '50vh'
+			},
+			borderRadius: {
+				'24': '24px'
+			},
+			fontSize: {
+				'footer': '0.9rem'
 			}
 		}
 	}
