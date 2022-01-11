a876171131710304f1f2d2922e6a42ceeff0ad3e
raf <itsashelf@gmail.com>
2022-01-11 21:45:35 +0300

    mistakes were made

--- flameshot_example.sh
+++ flameshot_example.sh
@@ -7,19 +7,18 @@ DOMAIN="" # Your upload domain (without http:// or https://)
 flameshot config -f "$IMAGENAME" # Make sure that Flameshot names the file correctly
 flameshot gui -r -p "$IMAGEPATH" # Prompt the screenshot GUI
 
-FILE=$FILE/$IMAGENAME # Fİle path and file name combined
+FILE="$IMAGEPATH$IMAGENAME.png" # File path and file name combined
 
-if test -f "$FILE/$IMAGENAME"; then # Check if file exists to handle Curl and rm errors
+# Check if file exists to handle Curl and rm errors
+if test -f "$FILE"; then
+  # Then upload the image and copy the response URL
+  URL=$(curl -X POST \
+    -H "Content-Type: multipart/form-data" \
+    -H "Accept: application/json" \
+    -H "User-Agent: ShareX/13.4.0" \
+    -H "Authorization: $KEY" \
+    -F "file=@$FILE" "https://$DOMAIN/" | grep -Po '(?<="resource":")[^"]+')
 
-# Upload the image and copy the response URL
-    URL=$(curl -X POST \
-      -H "Content-Type: multipart/form-data" \
-      -H "Accept: application/json" \
-      -H "User-Agent: ShareX/13.4.0" \
-      -H "Authorization: $KEY" \
-      -F "file=@$IMAGEPATH$IMAGENAME.png" "https://$DOMAIN/" | grep -Po '(?<="resource":")[^"]+')
-    rm "$FILE"
-fi
-# Echo instead of printf as echo is marginally faster than printf
-echo -e "$URL" | xclip -sel clip
-
+  echo -n "$URL" | xclip -sel clip # Uses echo instead of printf as echo is marginally faster than printf
+  rm "$FILE"
+fi 
