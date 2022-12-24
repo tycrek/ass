ac8c861bb100805b7850e680ef56e3a54fec4e08
tycrek <gpg+github@jmoore.dev>
2022-12-24 16:51:37 -0700

    feat: add middleware function for verifying admin users

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -7,9 +7,20 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { findFromToken, users } from '../auth';
 import { data } from '../data';
+import { User } from '../types/auth';
 
+/**
+ * The primary API router
+ */
 const RouterApi = Router();
 
+/**
+ * Token authentication middleware for Admins
+ */
+const adminAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	const user = findFromToken(req.headers.authorization ?? '');
+	(user && user.admin) ? next() : res.sendStatus(401);
+};
 function buildUserRouter() {
 	const RouterUser = Router();
 
