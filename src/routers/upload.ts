338f6a73e1078e180a12bcb69f67753c1e1aaec2
tycrek <gpg+github@jmoore.dev>
2022-12-24 18:54:35 -0700

    feat: switch to `@tycrek/discord-hookr` for webhooks

--- src/routers/upload.ts
+++ src/routers/upload.ts
@@ -5,7 +5,8 @@ import fs from 'fs-extra';
 import bb from 'express-busboy';
 //const rateLimit = require('express-rate-limit');
 import { DateTime } from 'luxon';
-import { Webhook, MessageBuilder } from 'discord-webhook-node';
+import { Webhook, EmbedBuilder } from '@tycrek/discord-hookr';
+
 import { processUploaded } from '../storage';
 import { path, log, getTrueHttp, getTrueDomain, generateId, formatBytes } from '../utils';
 import { data } from '../data';
@@ -127,19 +128,17 @@ router.post('/', (req: Request, res: Response, next: Function) => {
 				hook.setAvatar(avatar);
 
 				// Build the embed
-				const embed = new MessageBuilder()
+				const embed = new EmbedBuilder()
 					.setTitle(logInfo)
-					// @ts-ignore
-					.setURL(resourceUrl) // I don't know why this is throwing an error when `setUrl` is used but it does. This is a workaround.
+					.setURL(resourceUrl)
 					.setDescription(`${admin ? `**User:** \`${uploader}\`\n` : ''}**Size:** \`${formatBytes(req.file.size)}\`\n**[Delete](${deleteUrl})**`)
-					.setThumbnail(thumbnailUrl)
-					// @ts-ignore
+					.setThumbnail({ url: thumbnailUrl })
 					.setColor(req.file.vibrant)
 					.setTimestamp();
 
 				// Send the embed to the webhook, then delete the client after to free resources
 				log.debug(`Sending${admin ? ' admin' : ''} embed to webhook`);
-				hook.send(embed)
+				hook.addEmbed(embed).send()
 					.then(() => log.debug(`Webhook${admin ? ' admin' : ''} sent`))
 					.catch((err) => log.error('Webhook error').err(err));
 			}
