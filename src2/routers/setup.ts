5d1d4a402e7c45fab7d6c3ce793ff09f1fd156d2
Josh Moore <josh.moore+gpg@jmoore.dev>
2023-07-12 22:10:36 -0600

    feat: migrated files to new backend directory

--- src2/routers/setup.ts
+++
@@ -1,8 +0,0 @@
-import { Router, json as BodyParserJson } from 'express';
-import { log } from '../log';
-
-const router = Router({ caseSensitive: true });
-
-router.get('/', (req, res) => res.render('setup'));
-
-export { router };
