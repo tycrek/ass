0d01fb670b879c8cf20a0c0b2f371a903f0a8a79
Josh Moore <josh.moore+gpg@jmoore.dev>
2023-07-14 17:38:11 -0600

    feat: sample for oggy

---
+++ backend/s3.ts
@@ -0,0 +1,14 @@
+/**
+ * Uploads a file to your configured S3 provider
+ */
+export const uploadFileS3 = () => { }
+
+/**
+ * Gets a file from your configured S3 provider
+ */
+export const getFileS3 = () => { }
+
+/**
+ * Deletes a file from your configured S3 provider
+ */
+export const deleteFileS3 = () => { }
