0a45ddcd3773f8d6c671e0ba227d0551c1eba501
Josh Moore <josh.moore+gpg@jmoore.dev>
2023-07-16 20:45:25 -0600

    feat: implement SQL mode switch

--- backend/data.ts
+++ backend/data.ts
@@ -3,6 +3,7 @@ import { path } from '@tycrek/joint';
 import { nanoid } from 'nanoid';
 import { log } from './log';
 import { AssFile, AssUser, NID, FilesSchema, UsersSchema } from 'ass';
+import { UserConfig } from './UserConfig';
 
 /**
  * Switcher type for exported functions
@@ -15,6 +16,11 @@ type DataSector = 'files' | 'users';
 const PATHS = {
 	files: path.join('.ass-data/files.json'),
 	users: path.join('.ass-data/users.json')
+};
+
+const bothWriter = async (files: FilesSchema, users: UsersSchema) => {
+	await fs.writeJson(PATHS.files, files, { spaces: '\t' });
+	await fs.writeJson(PATHS.users, users, { spaces: '\t' });
 };
 
 /**
@@ -64,4 +70,34 @@ export const ensureFiles = (): Promise<void> => new Promise(async (resolve, reje
 		log.error('Failed to verify existence of data files');
 		reject(err);
 	}
+});
+
+export const setDataModeToSql = (): Promise<void> => new Promise(async (resolve, reject) => {
+	log.debug('Setting data mode to SQL');
+
+	// Main config check
+	if (!UserConfig.ready || !UserConfig.config.sql?.mySql) return reject(new Error('MySQL not configured'));
+	const mySqlConf = UserConfig.config.sql.mySql;
+
+	// Read data files
+	const [files, users]: [FilesSchema, UsersSchema] = await Promise.all([fs.readJson(PATHS.files), fs.readJson(PATHS.users)]);
+
+	// Check the MySQL configuration
+	const checker = (val: string) => val != null && val !== '';
+	const issue =
+		!checker(mySqlConf.host) ? 'Missing MySQL Host'
+			: !checker(mySqlConf.user) ? 'Missing MySQL User'
+				: !checker(mySqlConf.password) ? 'Missing MySQL Password'
+					: !checker(mySqlConf.database) ? 'Missing MySQL Database'
+
+						// ! Blame VS Code for this weird indentation
+						: undefined;
+
+	// Set the vars
+	files.useSql = issue == null;
+	users.useSql = issue == null;
+
+	// Write data & return
+	await bothWriter(files, users);
+	(issue) ? reject(new Error(issue)) : resolve(void 0);
 });
